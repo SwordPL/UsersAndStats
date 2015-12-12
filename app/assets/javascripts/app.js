462c898346f35449646b2d812771e3e7a31697c8
Paulina <paulina.winkowska@gmail.com>
2015-12-12 21:52:42 +0100

    Groups template

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,13 +11,39 @@ usersAndStats.config(['$routeProvider',
                 templateUrl: "index.html",
                 controller: 'MainController'
             }
+        ).when('/groups/', {
+                templateUrl: "groups.html",
+                controller: 'MainController'
+            }
         )
     }
 ]);
 
+
+
 controllers = angular.module('controllers',[]);
 
 controllers.controller("MainController", [ '$scope', function($scope) {
+        $scope.yourSubjects = [
+            {   
+                id: 1,
+                name: "PSI"
+            },
+            {
+                id: 2,
+                name: "Sieci"
+            }
+        ]
 
+        $scope.otherSubjects = [
+            {   
+                id: 3,
+                name: "Toizo"
+            },
+            {
+                id: 4,
+                name: "Analiza"
+            }
+        ]
     }
 ]);
