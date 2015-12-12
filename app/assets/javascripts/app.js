94c57761afc02b11afaebe0be7abbd6df3348264
Paulina <paulina.winkowska@gmail.com>
2015-12-12 23:00:51 +0100

    Subject page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -15,6 +15,10 @@ usersAndStats.config(['$routeProvider',
                 templateUrl: "groups.html",
                 controller: 'MainController'
             }
+        ).when('/subject/', {
+                templateUrl: "subject.html",
+                controller: 'SubjectController'
+            }
         )
     }
 ]);
@@ -47,3 +51,37 @@ controllers.controller("MainController", [ '$scope', function($scope) {
         ]
     }
 ]);
+
+controllers.controller("SubjectController", [ '$scope', function($scope) {
+    $scope.subject = {
+        id: 1,
+        name: "PSI"
+    }
+
+    $scope.tasks = [
+        {   
+            id: 1,
+            name: "Diagramy 1.",
+            maxPoints: 20,
+            subject: "PSI",
+            path: 'xxx'
+        },
+        {   
+            id: 2,
+            name: "Diagramy 2.",
+            maxPoints: 20,
+            subject: "PSI",
+            path: 'xxx'
+        }
+    ]
+
+    $scope.newTasks = [
+        {   
+            id: 3,
+            name: "Diagramy 3.",
+            maxPoints: 20,
+            subject: "PSI",
+            path: 'xxx'
+        },
+    ]
+}]);
