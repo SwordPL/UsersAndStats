68fa02dd497415e2d70e68ed8a1fc24bb1b3d831
Hubert Pomorski <pomorski.hubert@gmail.com>
2015-12-12 14:43:00 +0100

    Introduced modular template AngularJs approach

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,23 @@
-angular.module('UsersAndStats', []);
+
+usersAndStats = angular.module('UsersAndStats',[
+    'templates',
+    'ngRoute',
+    'controllers'
+]);
+
+usersAndStats.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+                templateUrl: "index.html",
+                controller: 'MainController'
+            }
+        )
+    }
+]);
+
+controllers = angular.module('controllers',[]);
+
+controllers.controller("MainController", [ '$scope', function($scope) {
+
+    }
+]);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
+//= require angular-rails-templates
+//= require angular-route/angular-route
+//= require_tree ./templates
 //= require_tree .
