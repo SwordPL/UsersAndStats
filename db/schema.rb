58ffe0aede2259d862e5a9181981adcaa56e1c40
Hubert Pomorski <pomorski.hubert@gmail.com>
2015-12-19 16:38:02 +0100

    Added new fields to user: Name & Surname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218205823) do
+ActiveRecord::Schema.define(version: 20151219152700) do
 
   create_table "solutions", force: :cascade do |t|
     t.integer  "user_id",                limit: 4
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20151218205823) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
+    t.string   "surname",                limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
