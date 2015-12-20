2df7a8fb3c43c7472f4e4ab33d84f32dacaac89b
Paulina <paulina.winkowska@gmail.com>
2015-12-20 15:57:43 +0100

    Task solutions table

--- app/assets/javascripts/tasks/taskController.js
+++ app/assets/javascripts/tasks/taskController.js
@@ -12,6 +12,37 @@ controllers.controller("TaskController", [ '$scope', 'Auth', function($scope, Au
             path: 'xxx'
     };
 
+    $scope.userSolutions = [
+        {
+            id: 1,
+            compilationSuccess: false,
+            executionTime: 0,
+            points: 0,
+            createdAt: new Date(2015, 12, 1, 14, 15, 0, 0)
+        },
+        {
+            id: 2,
+            compilationSuccess: true,
+            executionTime: 100,
+            points: 10,
+            createdAt: new Date(2015, 12, 4, 15, 15, 0, 0)
+        },
+        {
+            id: 3,
+            compilationSuccess: true,
+            executionTime: 100,
+            points: 20,
+            createdAt: new Date(2015, 12, 10, 9, 15, 0, 0)      
+        },
+        {
+            id: 4,
+            compilationSuccess: true,
+            executionTime: 50,
+            points: 20,
+            createdAt: new Date(2015, 12, 12, 23, 15, 0, 0)
+        }
+    ];
+
     $scope.signedIn = Auth.isAuthenticated;
 
     Auth.currentUser().then(function (user){
