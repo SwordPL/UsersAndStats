698863ead4b2b296ce41ec894e106a4a1873ee26
Paulina <paulina.winkowska@gmail.com>
2015-12-20 16:21:42 +0100

    Ranking table

--- app/assets/javascripts/tasks/taskController.js
+++ app/assets/javascripts/tasks/taskController.js
@@ -1,4 +1,14 @@
 controllers.controller("TaskController", [ '$scope', 'Auth', function($scope, Auth) {
+    // $scope.signedIn = Auth.isAuthenticated;
+
+    // Auth.currentUser().then(function (user){
+    //     $scope.user = user;
+    // });
+
+    $scope.user = {
+        id: 1
+    }
+
     $scope.subject = {
         id: 1,
         name: "PSI"
@@ -42,11 +52,39 @@ controllers.controller("TaskController", [ '$scope', 'Auth', function($scope, Au
             createdAt: new Date(2015, 12, 12, 23, 15, 0, 0)
         }
     ];
-
-    $scope.signedIn = Auth.isAuthenticated;
-
-    Auth.currentUser().then(function (user){
-        $scope.user = user;
-    });
-        
+     
+    $scope.bestSolutions = [
+        {
+            id: 6,
+            author: 3,
+            compilationSuccess: true,
+            executionTime: 50,
+            points: 18,
+            createdAt: new Date(2015, 12, 10, 23, 15, 0, 0)
+        },
+        {
+            id: 7,
+            author: 4,
+            compilationSuccess: false,
+            executionTime: 0,
+            points: 0,
+            createdAt: new Date(2015, 12, 15, 23, 15, 0, 0)
+        },
+        {
+            id: 4,
+            author: 1,
+            compilationSuccess: true,
+            executionTime: 50,
+            points: 20,
+            createdAt: new Date(2015, 12, 16, 23, 15, 0, 0)
+        },
+        {
+            id: 5,
+            author: 2,
+            compilationSuccess: true,
+            executionTime: 20,
+            points: 20,
+            createdAt: new Date(2015, 12, 12, 23, 15, 0, 0)
+        }
+    ]   
 }]);
