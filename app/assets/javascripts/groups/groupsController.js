69949a28f507310fd75a8d214a8ec91fc0bce6d6
Paulina <paulina.winkowska@gmail.com>
2015-12-21 21:27:18 +0100

    New group panel added

--- app/assets/javascripts/groups/groupsController.js
+++ app/assets/javascripts/groups/groupsController.js
@@ -1,4 +1,15 @@
 controllers.controller("GroupsController", [ '$scope', 'groups', function($scope, groups) {
     $scope.yourSubjects = groups.yourSubjects;
     $scope.otherSubjects = groups.otherSubjects;
+
+    $scope.newGroupPanel = false;
+
+    $scope.addNewGroupPanel = function() {
+    	$scope.newGroupPanel = true;
+    }
+
+    $scope.addNewGroup = function() {
+    	$scope.newGroupPanel = false;
+    	alert("added");
+    }
 }]);
