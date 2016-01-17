df82163afc8c90820ad24653909c6ccb915eefc8
Hubert Pomorski <pomorski.hubert@gmail.com>
2016-01-17 20:54:56 +0100

    Sockets redone

--- app/assets/javascripts/solutions/solutionUploader.js
+++ app/assets/javascripts/solutions/solutionUploader.js
@@ -1,13 +1,16 @@
 angular.module('UsersAndStats').
 factory('solutionUploader', ['$websocket', function ($websocket) {
     return {
-        sendSolution: function(task, author, file) {
-            var dataStream = $websocket('wss://website.com/data');
+        sendSolution: function(task, author, file, input, output) {
+            var dataStream = $websocket('ws://localhost:8026/file-uploader/user-and-stats');
             dataStream.send(JSON.stringify(
                 { 
-                    task: task,
-                    author: author,
-                    file: file
+                    task_number: task,
+                    user_id: author,
+                    file: file,
+                    input: input,
+                    output: output,
+                    timeout: 60
                 }));
             dataStream.close();
         } 
